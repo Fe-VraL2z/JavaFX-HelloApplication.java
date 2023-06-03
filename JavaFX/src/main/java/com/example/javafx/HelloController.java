f0860919b06756fb1317697adc8c63bf15236a6a
FRANCISCO EDUARDO VARGAS R <124694081+Fe-VraL2z@users.noreply.github.com>
2023-06-03 12:22:03 -0600

    Add files via upload

--- JavaFX/src/main/java/com/example/javafx/HelloController.java
+++ JavaFX/src/main/java/com/example/javafx/HelloController.java
@@ -1,16 +1,33 @@
 package com.example.javafx;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
+import java.io.IOException;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class HelloController extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("hello-view.fxml"));
+        VBox root = loader.load();
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        Scene scene = new Scene(root, 400, 600);
+        String css = getClass().getResource("styles/app.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        stage.setTitle("Hola FX + CSS");
+        stage.setScene(scene);
+        stage.show();
     }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+    //identificar la etiqueta que va a cambiar
 }
